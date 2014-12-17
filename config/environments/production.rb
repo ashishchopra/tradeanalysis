70e05a9c2b6183d8c85af2c85ef78efb74dc0362
ashishchopra0308 <ashishchopra0308@gmail.com>
2014-12-17 20:50:49 +0530

    add devise authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
